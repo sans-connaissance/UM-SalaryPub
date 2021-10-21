e935a32366d119ad48dbb153dcd0b5c9566e21e1
sans-connaissance <davemalicke@gmail.com>
2021-10-21 10:41:00 -0400

    Initial Commit

---
+++ UMSalaryPubV4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UMSalaryPubV4
+//
+//  Created by David Malicke on 10/21/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UMSalaryPubV4/UMSalaryPubV4App.swift
@@ -0,0 +1,17 @@
+//
+//  UMSalaryPubV4App.swift
+//  UMSalaryPubV4
+//
+//  Created by David Malicke on 10/21/21.
+//
+
+import SwiftUI
+
+@main
+struct UMSalaryPubV4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
