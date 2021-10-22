0203ef024d0ba670d4fb9ddcfa81330428175607
sans-connaissance <davemalicke@gmail.com>
2021-10-22 12:39:00 -0400

    Preparing to upload test data

---
+++ UMSalaryPubV4/Utils/AppUtils.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+class AppUtils {
+    
+    static func jsonDataFromFile(name: String) -> Data {
+        let url = Bundle.main.url(forResource: name, withExtension: "json")!
+        return try! Data(contentsOf: url)
+    }
+    
+    static func dictionaryFromFile(name: String) -> [String: Any] {
+        let data = jsonDataFromFile(name: name)
+        return try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+    }
+}
