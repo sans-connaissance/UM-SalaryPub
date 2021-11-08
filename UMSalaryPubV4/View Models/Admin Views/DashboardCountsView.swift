a3e38d513ee156013470784c783ac270cea3e498
sans-connaissance <davemalicke@gmail.com>
2021-11-08 11:18:55 -0500

    ADD AdminViewModel funcs

--- UMSalaryPubV4/View Models/Admin Views/DashboardCountsView.swift
+++ UMSalaryPubV4/View Models/Admin Views/DashboardCountsView.swift
@@ -14,12 +14,7 @@ struct DashboardCountsView: View {
     @StateObject private var adminVM = AdminViewModel()
     
     var body: some View {
-        
-        //create a forEach here based on the adminVM.importYears.count
-        
-        // do I create a struct that represents the count? Like a PersonDetailView Struct or something or like a ViewCell of somekind
-        
-        
+         
         List {
             
             
@@ -29,107 +24,11 @@ struct DashboardCountsView: View {
                         let count = personCount.count
                         Text("Person Count: \(count)")
                     }
-                }
-                
-            }
-
-            ForEach(0..<adminVM.importYears.count) { _ in
-                if let year = adminVM.importYears.last {
-                   
-                    Section(header: Text(String(year))) {
-                    
-                    }
                     
                 }
-                
-    
                 
             }
             
-            VStack(alignment: .leading) {
-                Section(header: Text("2020")) {
-                    Text("Person Count: \(adminVM.persons2020.count)")
-                    Text("Title Count: \(adminVM.titles2020.count)")
-                    Text("Department Count: \(adminVM.departments2020.count)")
-                    Text("Campus Count: \(adminVM.campuses2020.count)")
-
-
-                }
-
-                Section(header: Text("2019")) {
-                    Text("Person Count: \(adminVM.persons2019.count)")
-                    Text("Title Count: \(adminVM.titles2019.count)")
-                    Text("Department Count: \(adminVM.departments2019.count)")
-                    Text("Campus Count: \(adminVM.campuses2019.count)")
-
-
-                }
-
-                Section(header: Text("2018")) {
-                    Text("Person Count: \(adminVM.persons2018.count)")
-                    Text("Title Count: \(adminVM.titles2018.count)")
-                    Text("Department Count: \(adminVM.departments2018.count)")
-                    Text("Campus Count: \(adminVM.campuses2018.count)")
-
-
-                }
-
-                Section(header: Text("2017")) {
-                    Text("Person Count: \(adminVM.persons2017.count)")
-                    Text("Title Count: \(adminVM.titles2017.count)")
-                    Text("Department Count: \(adminVM.departments2017.count)")
-                    Text("Campus Count: \(adminVM.campuses2017.count)")
-
-
-                }
-
-                Section(header: Text("2016")) {
-                    Text("Person Count: \(adminVM.persons2016.count)")
-                    Text("Title Count: \(adminVM.titles2016.count)")
-                    Text("Department Count: \(adminVM.departments2016.count)")
-                    Text("Campus Count: \(adminVM.campuses2016.count)")
-
-
-                }
-
-                Section(header: Text("2015")) {
-                    Text("Person Count: \(adminVM.persons2015.count)")
-                    Text("Title Count: \(adminVM.titles2015.count)")
-                    Text("Department Count: \(adminVM.departments2015.count)")
-                    Text("Campus Count: \(adminVM.campuses2015.count)")
-
-
-                }
-
-                Section(header: Text("2014")) {
-                    if let personCount = adminVM.allPersons[2014] {
-                        let count = personCount.count
-                        Text("Person Count: \(count)")
-                    }
-
-                    Text("Title Count: \(adminVM.titles2014.count)")
-                    Text("Department Count: \(adminVM.departments2014.count)")
-                    Text("Campus Count: \(adminVM.campuses2014.count)")
-
-
-                }
-
-                Section(header: Text("2013")) {
-
-                    if let personCount = adminVM.allPersons[2013] {
-                        let count = personCount.count
-                        Text("Person Count: \(count)")
-                    }
-
-
-                    Text("Title Count: \(adminVM.titles2013.count)")
-                    Text("Department Count: \(adminVM.departments2013.count)")
-                    Text("Campus Count: \(adminVM.campuses2013.count)")
-
-
-                }
-                
-            }
         }
         .onAppear(perform: {
             adminVM.loadAdminScreen()
--- UMSalaryPubV4/View Models/AdminViewModel.swift
+++ UMSalaryPubV4/View Models/AdminViewModel.swift
@@ -15,456 +15,52 @@ class AdminViewModel: ObservableObject {
     
     
     @Published var allPersons = [Int: [PersonViewModel]]()
-    
-    @Published var personsAll = [PersonViewModel]()
-    @Published var persons2020 = [PersonViewModel]()
-    @Published var persons2019 = [PersonViewModel]()
-    @Published var persons2018 = [PersonViewModel]()
-    @Published var persons2017 = [PersonViewModel]()
-    @Published var persons2016 = [PersonViewModel]()
-    @Published var persons2015 = [PersonViewModel]()
-    @Published var persons2014 = [PersonViewModel]()
-    @Published var persons2013 = [PersonViewModel]()
-    
-    @Published var titlesAll = [TitleViewModel]()
-    @Published var titles2020 = [TitleViewModel]()
-    @Published var titles2019 = [TitleViewModel]()
-    @Published var titles2018 = [TitleViewModel]()
-    @Published var titles2017 = [TitleViewModel]()
-    @Published var titles2016 = [TitleViewModel]()
-    @Published var titles2015 = [TitleViewModel]()
-    @Published var titles2014 = [TitleViewModel]()
-    @Published var titles2013 = [TitleViewModel]()
-    
-    @Published var departmentsAll = [DepartmentViewModel]()
-    @Published var departments2020 = [DepartmentViewModel]()
-    @Published var departments2019 = [DepartmentViewModel]()
-    @Published var departments2018 = [DepartmentViewModel]()
-    @Published var departments2017 = [DepartmentViewModel]()
-    @Published var departments2016 = [DepartmentViewModel]()
-    @Published var departments2015 = [DepartmentViewModel]()
-    @Published var departments2014 = [DepartmentViewModel]()
-    @Published var departments2013 = [DepartmentViewModel]()
-    
-    //    @Published var departmentAllYears = [[DepartmentViewModel]]()
-    //    private var  departmentByYear = [Department]()
-    
-    @Published var campusesAll = [CampusViewModel]()
-    @Published var campuses2020 = [CampusViewModel]()
-    @Published var campuses2019 = [CampusViewModel]()
-    @Published var campuses2018 = [CampusViewModel]()
-    @Published var campuses2017 = [CampusViewModel]()
-    @Published var campuses2016 = [CampusViewModel]()
-    @Published var campuses2015 = [CampusViewModel]()
-    @Published var campuses2014 = [CampusViewModel]()
-    @Published var campuses2013 = [CampusViewModel]()
+    @Published var allTitles = [Int: [TitleViewModel]]()
+    @Published var allDepartments = [Int: [DepartmentViewModel]]()
+    @Published var allCampuses = [Int: [CampusViewModel]]()
     
     
     func loadAdminScreen() {
         
-        allPersonsGet()
-        
         getAllPersons()
-        //        getAllTitles()
-        //        getAllDepartments()
+        getAllTitles()
+        getAllDepartments()
+        getAllCampuses()
         
-        getPersons2020()
-        getPersons2019()
-        getPersons2018()
-        getPersons2017()
-        getPersons2016()
-        getPersons2015()
-        getPersons2014()
-        getPersons2013()
-        
-        getTitles2020()
-        getTitles2019()
-        getTitles2018()
-        getTitles2017()
-        getTitles2016()
-        getTitles2015()
-        getTitles2014()
-        getTitles2013()
-        
-        getDepartments2020()
-        getDepartments2019()
-        getDepartments2018()
-        getDepartments2017()
-        getDepartments2016()
-        getDepartments2015()
-        getDepartments2014()
-        getDepartments2013()
-        
-        getCampuses2020()
-        getCampuses2019()
-        getCampuses2018()
-        getCampuses2017()
-        getCampuses2016()
-        getCampuses2015()
-        getCampuses2014()
-        getCampuses2013()
     }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    //MARK: ------------------------ Person ------------------------
     func getAllPersons() {
         
-        let personsAll: [Person] = Person.all()
-        DispatchQueue.main.async {
-            self.personsAll = personsAll.map(PersonViewModel.init)
-        }
-    }
-    
-    func allPersonsGet() {
-        
         for year in importYears {
-            
             let personViewModel: [Person] = Person.byYear(year: String(year))
-            
             allPersons[year] = personViewModel.map(PersonViewModel.init)
-            
         }
-        
     }
-    
-    func getPersons2020() {
-        let persons2020: [Person] = Person.byYear(year: "2020")
-        
-        DispatchQueue.main.async {
-            self.persons2020 = persons2020.map(PersonViewModel.init)
-        }
-        
-    }
-    
-    func getPersons2019() {
-        let persons2019: [Person] = Person.byYear(year: "2019")
-        
-        DispatchQueue.main.async {
-            self.persons2019 = persons2019.map(PersonViewModel.init)
-        }
-        
-    }
-    
-    func getPersons2018() {
-        let persons2018: [Person] = Person.byYear(year: "2018")
-        
-        DispatchQueue.main.async {
-            self.persons2018 = persons2018.map(PersonViewModel.init)
-        }
-        
-    }
-    
-    func getPersons2017() {
-        let persons2017: [Person] = Person.byYear(year: "2017")
-        
-        DispatchQueue.main.async {
-            self.persons2017 = persons2017.map(PersonViewModel.init)
-        }
-        
-    }
-    
-    func getPersons2016() {
-        let persons2016: [Person] = Person.byYear(year: "2016")
-        
-        DispatchQueue.main.async {
-            self.persons2016 = persons2016.map(PersonViewModel.init)
-        }
-        
-    }
-    
-    func getPersons2015() {
-        let persons2015: [Person] = Person.byYear(year: "2015")
-        
-        DispatchQueue.main.async {
-            self.persons2015 = persons2015.map(PersonViewModel.init)
-        }
-        
-    }
-    
-    func getPersons2014() {
-        let persons2014: [Person] = Person.byYear(year: "2014")
-        
-        DispatchQueue.main.async {
-            self.persons2014 = persons2014.map(PersonViewModel.init)
-        }
-        
-    }
-    
-    
-    func getPersons2013() {
-        let persons2013: [Person] = Person.byYear(year: "2013")
-        
-        DispatchQueue.main.async {
-            self.persons2013 = persons2013.map(PersonViewModel.init)
-        }
-        
-    }
-    
-    
-    //MARK: ------------------------ Titles ------------------------
     
     func getAllTitles() {
-        let titlesAll: [Title] = Title.all()
-        DispatchQueue.main.async {
-            self.titlesAll = titlesAll.map(TitleViewModel.init)
+        
+        for year in importYears {
+            let titleViewModel: [Title] = Title.byYear(year: String(year))
+            allTitles[year] = titleViewModel.map(TitleViewModel.init)
         }
     }
-    
-    func getTitles2020() {
-        let titles2020: [Title] = Title.byYear(year: "2020")
-        
-        DispatchQueue.main.async {
-            self.titles2020 = titles2020.map(TitleViewModel.init)
-        }
-        
-    }
-    
-    func getTitles2019() {
-        let titles2019: [Title] = Title.byYear(year: "2019")
-        
-        DispatchQueue.main.async {
-            self.titles2019 = titles2019.map(TitleViewModel.init)
-        }
-        
-    }
-    
-    func getTitles2018() {
-        let titles2018: [Title] = Title.byYear(year: "2018")
-        
-        DispatchQueue.main.async {
-            self.titles2018 = titles2018.map(TitleViewModel.init)
-        }
-        
-    }
-    
-    func getTitles2017() {
-        let titles2017: [Title] = Title.byYear(year: "2017")
-        
-        DispatchQueue.main.async {
-            self.titles2017 = titles2017.map(TitleViewModel.init)
-        }
-        
-    }
-    
-    func getTitles2016() {
-        let titles2016: [Title] = Title.byYear(year: "2016")
-        
-        DispatchQueue.main.async {
-            self.titles2016 = titles2016.map(TitleViewModel.init)
-        }
-        
-    }
-    
-    func getTitles2015() {
-        let titles2015: [Title] = Title.byYear(year: "2015")
-        
-        DispatchQueue.main.async {
-            self.titles2015 = titles2015.map(TitleViewModel.init)
-        }
-        
-    }
-    
-    func getTitles2014() {
-        let titles2014: [Title] = Title.byYear(year: "2014")
-        
-        DispatchQueue.main.async {
-            self.titles2014 = titles2014.map(TitleViewModel.init)
-        }
-        
-    }
-    
-    func getTitles2013() {
-        let titles2013: [Title] = Title.byYear(year: "2013")
-        
-        DispatchQueue.main.async {
-            self.titles2013 = titles2013.map(TitleViewModel.init)
-        }
-        
-    }
-    
-    
-    
-    //MARK: ------------------------ Departments ---------------------
     
     func getAllDepartments() {
-        let departmentsAll: [Department] = Department.all()
-        DispatchQueue.main.async {
-            self.departmentsAll = departmentsAll.map(DepartmentViewModel.init)
-        }
         
+        for year in importYears {
+            let departmentViewModel: [Department] = Department.byYear(year: String(year))
+            allDepartments[year] = departmentViewModel.map(DepartmentViewModel.init)
+        }
     }
     
-    
-    func getDepartments2020() {
-        let departments2020: [Department] = Department.byYear(year: "2020")
+    func getAllCampuses() {
         
-        DispatchQueue.main.async {
-            self.departments2020 = departments2020.map(DepartmentViewModel.init)
+        for year in importYears {
+            let campusViewModel: [Campus] = Campus.byYear(year: String(year))
+            allCampuses[year] = campusViewModel.map(CampusViewModel.init)
         }
-        
     }
     
-    func getDepartments2019() {
-        let departments2019: [Department] = Department.byYear(year: "2019")
-        
-        DispatchQueue.main.async {
-            self.departments2019 = departments2019.map(DepartmentViewModel.init)
-        }
-        
-    }
-    
-    func getDepartments2018() {
-        let departments2018: [Department] = Department.byYear(year: "2018")
-        
-        DispatchQueue.main.async {
-            self.departments2018 = departments2018.map(DepartmentViewModel.init)
-        }
-        
-    }
-    
-    func getDepartments2017() {
-        let departments2017: [Department] = Department.byYear(year: "2017")
-        
-        DispatchQueue.main.async {
-            self.departments2017 = departments2017.map(DepartmentViewModel.init)
-        }
-        
-    }
-    
-    func getDepartments2016() {
-        let departments2016: [Department] = Department.byYear(year: "2016")
-        
-        DispatchQueue.main.async {
-            self.departments2016 = departments2016.map(DepartmentViewModel.init)
-        }
-        
-    }
-    
-    func getDepartments2015() {
-        let departments2015: [Department] = Department.byYear(year: "2015")
-        
-        DispatchQueue.main.async {
-            self.departments2015 = departments2015.map(DepartmentViewModel.init)
-        }
-        
-    }
-    
-    func getDepartments2014() {
-        let departments2014: [Department] = Department.byYear(year: "2014")
-        
-        DispatchQueue.main.async {
-            self.departments2014 = departments2014.map(DepartmentViewModel.init)
-        }
-        
-    }
-    
-    func getDepartments2013() {
-        let departments2013: [Department] = Department.byYear(year: "2013")
-        
-        DispatchQueue.main.async {
-            self.departments2013 = departments2013.map(DepartmentViewModel.init)
-        }
-        
-    }
-    
-    //This is a great question to add to github -- do that after getting the rest of this to work the slow way.
-    //    func getAllDepartmentsByYear(years: [String]) {
-    //
-    //
-    //        for year in years {
-    //            let departmentByYear: [Department] = Department.byYear(year: year)
-    //
-    ////            DispatchQueue.main.async {
-    ////                self.departmentByYear = departmentByYear.map(DepartmentViewModel.init)
-    ////
-    ////            }
-    //        }
-    //    }
-    
-    
-    
-    //MARK: ------------------------ Campuses ---------------------
-    
-    func getCampuses2020() {
-        let campuses2020: [Campus] = Campus.byYear(year: "2020")
-        
-        DispatchQueue.main.async {
-            self.campuses2020 = campuses2020.map(CampusViewModel.init)
-        }
-        
-    }
-    
-    func getCampuses2019() {
-        let campuses2019: [Campus] = Campus.byYear(year: "2019")
-        
-        DispatchQueue.main.async {
-            self.campuses2019 = campuses2019.map(CampusViewModel.init)
-        }
-        
-    }
-    
-    func getCampuses2018() {
-        let campuses2018: [Campus] = Campus.byYear(year: "2018")
-        
-        DispatchQueue.main.async {
-            self.campuses2018 = campuses2018.map(CampusViewModel.init)
-        }
-        
-    }
-    
-    func getCampuses2017() {
-        let campuses2017: [Campus] = Campus.byYear(year: "2017")
-        
-        DispatchQueue.main.async {
-            self.campuses2017 = campuses2017.map(CampusViewModel.init)
-        }
-        
-    }
-    
-    func getCampuses2016() {
-        let campuses2016: [Campus] = Campus.byYear(year: "2016")
-        
-        DispatchQueue.main.async {
-            self.campuses2016 = campuses2016.map(CampusViewModel.init)
-        }
-        
-    }
-    
-    func getCampuses2015() {
-        let campuses2015: [Campus] = Campus.byYear(year: "2015")
-        
-        DispatchQueue.main.async {
-            self.campuses2015 = campuses2015.map(CampusViewModel.init)
-        }
-        
-    }
-    
-    func getCampuses2014() {
-        let campuses2014: [Campus] = Campus.byYear(year: "2014")
-        
-        DispatchQueue.main.async {
-            self.campuses2014 = campuses2014.map(CampusViewModel.init)
-        }
-        
-    }
-    
-    func getCampuses2013() {
-        let campuses2013: [Campus] = Campus.byYear(year: "2013")
-        
-        DispatchQueue.main.async {
-            self.campuses2013 = campuses2013.map(CampusViewModel.init)
-        }
-        
-    }
-    
-    
-    
+  
 }
 
