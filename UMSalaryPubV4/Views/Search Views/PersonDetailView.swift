f2c35e3931973c2809e5aab30f17ec4dbc41f8d3
sans-connaissance <davemalicke@gmail.com>
2021-11-15 14:39:05 -0500

    ADD PersonDetailView Structs

--- UMSalaryPubV4/Views/Search Views/PersonDetailView.swift
+++ UMSalaryPubV4/Views/Search Views/PersonDetailView.swift
@@ -9,10 +9,7 @@ import SwiftUI
 
 struct PersonDetailView: View {
     
-    // this needs to be replaced with a PersonListVM or PersonDetailVM that has a FetchRequest by the Peron's name
-    // will like need to populate person with a variable passed from the PersonView
     @StateObject private var personDetailVM = PersonDetailViewModel()
-    
     let person: PersonViewModel
     
     var body: some View {
@@ -27,10 +24,87 @@ struct PersonDetailView: View {
                 }
             }
             
-        } .onAppear(perform: { personDetailVM.getPersons(vm: person)
-        })
+        }
+        .onAppear(perform: {personDetailVM.getPersons(vm: person)})
     }
 }
+
+
+struct PersonDetailRowRight: View {
+    
+    let person: PersonViewModel
+    
+    var body: some View {
+        
+        VStack(alignment: .leading) {
+            Group{
+                Text("Annual Full-Time Rate (FTR)").textStyle(SmallGrey())
+                Text(person.apptAnnualFTR).textStyle(DetailData())
+            }
+            Spacer()
+            Group {
+                Text("Appointment FTR Basis").textStyle(SmallGrey())
+                Text(person.apptFTRBasis).textStyle(DetailData())
+            }
+            Spacer()
+            Group{
+                Text("Appointment Fraction").textStyle(SmallGrey())
+                Text(person.apptFraction).textStyle(DetailData())
+            }
+            Spacer()
+            Group{
+                Text("Amount from General Fund").textStyle(SmallGrey())
+                Text(person.amtSalaryFromGeneralFund).textStyle(DetailData())
+            }
+            Spacer()
+        }
+    }
+}
+
+struct PersonDetailRowLeft: View {
+    
+    let person: PersonViewModel
+    
+    var body: some View {
+
+        VStack(alignment: .leading) {
+            Group{
+                
+                Text("Campus").textStyle(SmallGrey())
+                Text(person.campus).textStyle(DetailData())
+//                NavigationLink(destination: CampusDetailView(campusDetailViewPredicate1: person.campus?.campusName ?? "")){
+//                    Text(person.campus).textStyle(DetailData())
+//                }
+                
+            }
+            Spacer()
+            Group {
+                Text("Department").textStyle(SmallGrey())
+                Text(person.department).textStyle(DetailData())
+//                NavigationLink(destination: DepartmentDetailView(departmentDetailViewPredicate1: person.department?.departmentName ?? "")){
+//                    Text(person.department).textStyle(DetailData())
+//                }
+            }
+            Spacer()
+            Group{
+                Text("Title").textStyle(SmallGrey())
+                Text(person.title).textStyle(DetailData())
+//                NavigationLink(destination: TitleDetailView(titleDetailViewPredicate1: person.title?.titleName ?? "")){
+//                    Text(person.title).textStyle(DetailData())
+//                }
+            }
+            Spacer()
+            Group{
+                Text(" ").textStyle(SmallGrey())
+                Text(" ").textStyle(DetailData())
+            }
+            Spacer()
+        }
+    }
+}
+
+
+
 
 struct PersonDetailView_Previews: PreviewProvider {
     static var previews: some View {
