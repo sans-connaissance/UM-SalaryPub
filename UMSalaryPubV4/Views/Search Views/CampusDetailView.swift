058eba0d3a2a2ba80f30c01bf7728db77890e359
sans-connaissance <davemalicke@gmail.com>
2021-11-20 21:09:54 -0500

    ADD CampusDetailView

---
+++ UMSalaryPubV4/Views/Search Views/CampusDetailView.swift
@@ -0,0 +1,117 @@
+//
+//  CampusDetailView.swift
+//  UMSalaryPubV4
+//
+//  Created by David Malicke on 11/20/21.
+//
+
+import SwiftUI
+
+struct CampusDetailView: View {
+    
+    @StateObject private var vm = CampusDetailViewModel()
+    
+    let campus: CampusViewModel
+    
+    var body: some View {
+        
+        ScrollView{
+            
+            VStack(alignment: .center) {
+                ForEach(vm.campusesDetail.reversed(), id: \.self) { campus in
+                    Divider()
+                    
+                    Section(header: Text(String(campus.year)).bold()) {
+                        Divider()
+                        
+                        VStack {
+                            HStack(spacing: 5){
+                                Spacer()
+                                CampusDetailRowLeft(campus: campus)
+                                Spacer()
+                                CampusDetailRowRight(campus: campus)
+                                Spacer()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        .navigationTitle(campus.campusName)
+        .onAppear(perform: {vm.getCampuses(vm: campus)})
+    }
+}
+
+struct CampusDetailRowRight: View {
+    let campus: CampusViewModel
+    
+    var body: some View {
+        
+        VStack(alignment: .leading) {
+            Spacer()
+            Group{
+                Text("Avg. Annual FTR").textStyle(SmallGrey())
+                Text(campus.campusAverageAnnual).textStyle(DetailData())
+                
+            }
+            Spacer()
+            Group{
+                Text("Median Annual FTR").textStyle(SmallGrey())
+                Text(campus.campusMedianAnnual).textStyle(DetailData())
+            }
+            Spacer()
+            Group {
+                Text("Max Annual FTR").textStyle(SmallGrey())
+                Text(campus.campusMaxAnnual).textStyle(DetailData())
+            }
+            Spacer()
+            Group{
+                Text("Min Annual FTR").textStyle(SmallGrey())
+                Text(campus.campusMinAnnual).textStyle(DetailData())
+            }
+            Spacer()
+        }
+    }
+}
+
+
+struct CampusDetailRowLeft: View {
+    
+    let campus: CampusViewModel
+    
+    var body: some View {
+        
+        
+        VStack(alignment: .leading) {
+            Spacer()
+            Group{
+                Text("People on campus").textStyle(SmallGrey())
+                Text(campus.campusCount).textStyle(DetailData())
+            }
+            Spacer()
+            Group{
+                Text(" ").textStyle(SmallGrey())
+                Text(" ").textStyle(DetailData())
+            }
+            Spacer()
+            Group{
+                Text(" ").textStyle(SmallGrey())
+                Text(" ").textStyle(DetailData())
+            }
+            Spacer()
+            Group{
+                Text(" ").textStyle(SmallGrey())
+                Text(" ").textStyle(DetailData())
+            }
+            Spacer()
+        }
+    }
+}
+
+
+
+struct CampusDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        CampusDetailView(campus: CampusViewModel(campus: Campus(context: CoreDataManager.shared.viewContext)))
+    }
+}
