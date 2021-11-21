dd811c780523826c770f8dc545f3e261b50987ea
sans-connaissance <davemalicke@gmail.com>
2021-11-20 19:40:23 -0500

    ADD DepartmentDetailViewModel

---
+++ UMSalaryPubV4/View Models/DepartmentDetailViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  DepartmentDetailViewModel.swift
+//  UMSalaryPubV4
+//
+//  Created by David Malicke on 11/20/21.
+//
+
+import Foundation
+
+class DepartmentDetailViewModel: ObservableObject {
+    
+    @Published var departmentsDetail = [DepartmentViewModel]()
+    @Published var selectedKeyPath: NamePredicate = .Department
+    
+    func getDepartments(vm: DepartmentViewModel) {
+        let request: [Department] = Department.byName(keyPath: selectedKeyPath.returnText, name: vm.departmentName)
+        departmentsDetail = request.map(DepartmentViewModel.init)
+        
+    }
+}
