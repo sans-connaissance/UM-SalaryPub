b37cf9de2a28bf9a9c2a8045377462f208da5219
sans-connaissance <davemalicke@gmail.com>
2021-11-20 19:38:32 -0500

    ADD TitleDetailView

---
+++ UMSalaryPubV4/View Models/TitleDetailViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  TitleDetailViewModel.swift
+//  UMSalaryPubV4
+//
+//  Created by David Malicke on 11/20/21.
+//
+
+import Foundation
+
+class TitleDetailViewModel: ObservableObject {
+    
+    @Published var titlesDetail = [TitleViewModel]()
+    @Published var selectedKeyPath: NamePredicate = .Title
+    
+    func getPersons(vm: TitleViewModel) {
+        let request: [Title] = Title.byName(keyPath: selectedKeyPath.returnText, name: vm.titleName)
+        titlesDetail = request.map(TitleViewModel.init)
+        
+    }
+}
