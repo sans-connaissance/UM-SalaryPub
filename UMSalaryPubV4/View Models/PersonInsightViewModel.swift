8dcd3ec852054ac979961ba4e282e2a7188c74c2
sans-connaissance <davemalicke@gmail.com>
2021-11-22 22:28:23 -0500

    ADD Charts Package Dependency

--- UMSalaryPubV4/View Models/PersonInsightViewModel.swift
+++ UMSalaryPubV4/View Models/PersonInsightViewModel.swift
@@ -13,24 +13,39 @@ class PersonInsightViewModel: ObservableObject {
     @Published var salaries = [Double]()
     @Published var titlesInsight = [TitleViewModel]()
     @Published var departmentsInsight = [DepartmentViewModel]()
-    
-    
-    
-    private var personsKeyPath: NamePredicate = .Person
+    @Published var campusesInsight = [CampusViewModel]()
+
+    private var personsKP: NamePredicate = .Person
     private var titleNameKP: NamePredicate = .Title
     private var titleYearKP: YearByType = .Title
+    private var departmentNameKP: NamePredicate = .Department
+    private var departmentYearKP: YearByType = .Department
+    private var campusNameKP: NamePredicate = .Campus
+    private var campusYearKP: YearByType = .Campus
+    
+    
     
     func getPersons(vm: PersonViewModel) {
-        let request: [Person] = Person.byName(keyPath: personsKeyPath.returnText, name: vm.fullName)
+        let request: [Person] = Person.byName(keyPath: personsKP.returnText, name: vm.fullName)
         personsInsight = request.map(PersonViewModel.init)
     }
     
-// need to add another one for the name or year predicate in the baseClass
     func getTitles(vm: PersonViewModel) {
         let request: [Title] = Title.forInsights(nameKeyPath: titleNameKP.returnText, yearKeyPath: titleYearKP.returnText,  name: vm.title, year: vm.year)
         titlesInsight = request.map(TitleViewModel.init)
     }
     
+    func getDepartments(vm: PersonViewModel) {
+        let request: [Department] = Department.forInsights(nameKeyPath: departmentNameKP.returnText, yearKeyPath: departmentYearKP.returnText,  name: vm.title, year: vm.year)
+        departmentsInsight = request.map(DepartmentViewModel.init)
+        
+    }
+    
+    func getCampuses(vm: PersonViewModel) {
+        let request: [Campus] = Campus.forInsights(nameKeyPath: campusNameKP.returnText, yearKeyPath: campusNameKP.returnText,  name: vm.title, year: vm.year)
+        campusesInsight = request.map(CampusViewModel.init)
+        
+    }
     
     
     func getPercentChange(array: [PersonViewModel]) {
