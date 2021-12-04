b373b9118feeded5c950ca25032301354420d26e
sans-connaissance <davemalicke@gmail.com>
2021-12-04 15:25:59 -0500

    DELETE unused button structs

--- UMSalaryPubV4/Views/Buttons/ButtonViews.swift
+++ UMSalaryPubV4/Views/Buttons/ButtonViews.swift
@@ -8,30 +8,6 @@
 import SwiftUI
 
 
-struct SortByMoneyDescendingButton: View {
-    
-    @Binding var sortByMoneyDescending: Bool
-    @Binding var sortByMoneyAscending: Bool
-    @Binding var sortAlphabetically: Bool
-    @Binding var sortByPersonCountDescending: Bool
-    @Binding var sortByPersonCountAscending: Bool
-    
-    var body: some View {
-        Button {
-            sortByMoneyDescending = true
-            sortByMoneyAscending = false
-            sortAlphabetically = false
-            sortByPersonCountDescending = false
-            sortByPersonCountAscending = false
-            
-        } label: {
-            Text("$$$")
-                .foregroundColor(sortByMoneyDescending ? .blue : .gray)
-        }
-        
-    }
-}
-
 struct SortListButton: View {
     var selected: SortOption
     // Should sortButtons be a type?
@@ -83,99 +59,6 @@ enum SortOption: String, CaseIterable {
 
 
 
-struct SortByMoneyAscendingButton: View {
-    
-    @Binding var sortByMoneyDescending: Bool
-    @Binding var sortByMoneyAscending: Bool
-    @Binding var sortAlphabetically: Bool
-    @Binding var sortByPersonCountDescending: Bool
-    @Binding var sortByPersonCountAscending: Bool
-    
-    var body: some View {
-        Button {
-            sortByMoneyDescending = false
-            sortByMoneyAscending = true
-            sortAlphabetically = false
-            sortByPersonCountDescending = false
-            sortByPersonCountAscending = false
-            
-        } label: {
-            Text("$")
-                .foregroundColor(sortByMoneyAscending ? .blue : .gray)
-        }
-    }
-}
-
-
-struct SortAlphabeticallyButton: View {
-    
-    @Binding var sortByMoneyDescending: Bool
-    @Binding var sortByMoneyAscending: Bool
-    @Binding var sortAlphabetically: Bool
-    @Binding var sortByPersonCountDescending: Bool
-    @Binding var sortByPersonCountAscending: Bool
-    
-    var body: some View {
-        Button {
-            sortByMoneyDescending = false
-            sortByMoneyAscending = false
-            sortAlphabetically = true
-            sortByPersonCountDescending = false
-            sortByPersonCountAscending = false
-            
-        } label: {
-            Text("abc")
-                .foregroundColor(sortAlphabetically ? .blue : .gray)
-        }
-        
-    }
-}
-
-struct SortByPersonCountDescending: View {
-    
-    @Binding var sortByMoneyDescending: Bool
-    @Binding var sortByMoneyAscending: Bool
-    @Binding var sortAlphabetically: Bool
-    @Binding var sortByPersonCountDescending: Bool
-    @Binding var sortByPersonCountAscending: Bool
-    
-    var body: some View {
-        Button {
-            sortByMoneyDescending = false
-            sortByMoneyAscending = false
-            sortAlphabetically = false
-            sortByPersonCountDescending = true
-            sortByPersonCountAscending = false
-            
-        } label: {
-            Image(systemName: "person.3.fill")
-                .foregroundColor(sortByPersonCountDescending ? .blue : .gray)
-        }
-    }
-}
-
-struct SortByPersonCountAscending: View {
-    
-    @Binding var sortByMoneyDescending: Bool
-    @Binding var sortByMoneyAscending: Bool
-    @Binding var sortAlphabetically: Bool
-    @Binding var sortByPersonCountDescending: Bool
-    @Binding var sortByPersonCountAscending: Bool
-    
-    var body: some View {
-        Button {
-            sortByMoneyDescending = false
-            sortByMoneyAscending = false
-            sortAlphabetically = false
-            sortByPersonCountDescending = false
-            sortByPersonCountAscending = true
-            
-        } label: {
-            Image(systemName: "person.fill")
-                .foregroundColor(sortByPersonCountAscending ? .blue : .gray)
-        }
-    }
-}
 
 struct CloseInsightView: View {
     
