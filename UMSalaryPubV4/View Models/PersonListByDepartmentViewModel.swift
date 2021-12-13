e094ac5eab53238d36dda9e0cbcd28f5f191dd9f
sans-connaissance <davemalicke@gmail.com>
2021-12-12 23:06:24 -0500

    REFACTOR PersonListByDepartmentView

--- UMSalaryPubV4/View Models/PersonListByDepartmentViewModel.swift
+++ UMSalaryPubV4/View Models/PersonListByDepartmentViewModel.swift
@@ -11,7 +11,7 @@ import Foundation
 class PersonListByDepartmentViewModel: ObservableObject {
     @Published var personsByTitle = [PersonViewModel]()
     @Published var sortButtons = [SortOption: Bool]()
-    
+    @Published var firstAppear = true
     func getPersonsByDepartment(vm: DepartmentViewModel) {
         let request: [Person] = Person.byDepartment(
             year: String(vm.department.departmentYear),
@@ -24,9 +24,15 @@ class PersonListByDepartmentViewModel: ObservableObject {
     }
     
     func setButtons() {
-        SortOption.allCases.forEach { button in
-            sortButtons[button] = false
+        if firstAppear {
+            SortOption.allCases.forEach { button in
+                sortButtons[button] = false
+            }
+            sortButtons[SortOption.sortByMoneyDescending] = true
         }
-        sortButtons[SortOption.sortByMoneyDescending] = true
+    }
+    
+    func flipFirstAppear() {
+        firstAppear = false
     }
 }
--- UMSalaryPubV4/Views/Search Views/PersonListByDepartmentView.swift
+++ UMSalaryPubV4/Views/Search Views/PersonListByDepartmentView.swift
@@ -11,14 +11,19 @@ struct PersonListByDepartmentView: View {
     @StateObject private var vm = PersonListByDepartmentViewModel()
     let department: DepartmentViewModel
 
+    
+    // CHECK HOLLY DAUL for messed up INSIGHTS BUTTON
+    //IS THERE A WAY TO USE LESS MEMORY?? MAYBE THE MEMORY USE ISNT THAT BAD
     var body: some View {
         VStack {
-            HStack {
-                ForEach(SortOption.personList, id: \.self) { button in
-                    SortListButton(selected: button, sortButtons: $vm.sortButtons)
-                        .onChange(of: vm.sortButtons) { _ in vm.getPersonsByDepartment(vm: department) }
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 15) {
+                    ForEach(SortOption.personList, id: \.self) { button in
+                        SortListButton(selected: button, sortButtons: $vm.sortButtons)
+                            .onChange(of: vm.sortButtons) { _ in vm.getPersonsByDepartment(vm: department) }
+                    }
                 }
-            }
+            }.padding([.leading, .trailing])
             Divider()
             List {
                 ForEach(vm.personsByTitle, id: \.self) { person in
@@ -27,18 +32,22 @@ struct PersonListByDepartmentView: View {
                     } label: {
                         PersonRow(person: person)
                     }
+                    .isDetailLink(true)
+                    .listRowBackground((Color(UIColor.systemBackground)))
                 }
             }
             .listStyle(GroupedListStyle())
-            .navigationTitle(department.departmentName)
-            .onAppear(perform: { vm.setButtons() })
-            .onAppear(perform: { vm.getPersonsByDepartment(vm: department) })
         }
+        .navigationTitle(department.departmentName)
+        .padding(.bottom)
+        .onAppear(perform: { vm.setButtons() })
+        .onAppear(perform: { vm.getPersonsByDepartment(vm: department) })
+        .onDisappear(perform: { vm.flipFirstAppear() })
     }
 }
 
-//struct PersonListByDepartmentView_Previews: PreviewProvider {
+// struct PersonListByDepartmentView_Previews: PreviewProvider {
 //    static var previews: some View {
 //        PersonListByDepartmentView()
 //    }
-//}
+// }
