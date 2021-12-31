762a52f9a9a9feacc4365f631f1ad0e1b763327e
sans-connaissance <davemalicke@gmail.com>
2021-12-30 22:32:22 -0500

    ADD MoreInfoView that disappeared

---
+++ UMSalaryPubV4/Views/More Info Views/MoreInfoView.swift
@@ -0,0 +1,68 @@
+//
+//  MoreInfoView.swift
+//  UMICHSalaryPub (iOS)
+//
+//  Created by David Malicke on 1/23/21.
+//
+
+import SwiftUI
+
+struct MoreInfoView: View {
+    var body: some View {
+        VStack {
+            Divider()
+            List {
+                Section(header: Text("Coming Soon")) {
+                    Text("More insights")
+                        .textStyle(DetailData())
+                }
+                Section(header: Text("Contact")) {
+                    Text("umichsalarypub@icloud.com")
+                        .textStyle(DetailData())
+                }
+
+                Section(header: Text("Definitions")) {
+                    NavigationLink(destination: GeneralFundView()) {
+                        Text("Amount from General Fund").font(.headline)
+                    }
+                    NavigationLink(destination: AnnualFTRView()) {
+                        Text("Annual Full-Time Rate (FTR)").font(.headline)
+                    }
+                    NavigationLink(destination: AppointmentFractionView()) {
+                        Text("Appointment Fraction").font(.headline)
+                    }
+                    NavigationLink(destination: FTRBasisView()) {
+                        Text("Appointment Full-Time Rate (FTR) Basis").font(.headline)
+                    }
+                }
+
+                Section(header: Text("Data")) {
+                    VStack(alignment: .leading) {
+                        Text("In accordance with the Freedom of Information Act, the University of Michigan is required to produce a publicly available annual Salary Disclosure Report. The publicly available Salary Disclosure Reports used in the production of UM SalaryPub are available at:").textStyle(DetailData())
+                        UMLinkView()
+                        Text("Insights and other statistics available in UM SalaryPub were derived by Frankfurt Industries, LLC., and are provided as-is, with no expressed or implied guarantees regarding the validity or accuracy of the content. UM SalaryPub is not affiliated with or endorsed by the University of Michigan or any other entity.").textStyle(DetailData())
+                    }
+                }
+
+            }.listStyle(GroupedListStyle())
+        }
+        .navigationTitle("More Info")
+        .padding()
+        .embedInNavigationView()
+        .navigationViewStyle(StackNavigationViewStyle())
+    }
+}
+
+struct UMLinkView: View {
+    var body: some View {
+        Link("HR Reports and Data Services", destination: URL(string: "https://hr.umich.edu/working-u-m/management-administration/hr-reports-data-services/hr-data-requests-standard-reports")!)
+            .font(.subheadline)
+    }
+}
+
+struct EmailLinkView: View {
+    var body: some View {
+        Link("umichsalarypub@icloud.com", destination: URL(string: "umichsalarypub@icloud.com")!)
+            .font(.subheadline)
+    }
+}
